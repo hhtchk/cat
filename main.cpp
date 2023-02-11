a0adcc3926bae1b99b84381dd873104941163055
hhtchk <125142247+hhtchk@users.noreply.github.com>
2023-02-11 19:26:51 +0300

    списочек

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include <cstdlib>
+#include <iterator>
+#include <list>
 using namespace std;
-int main(){
-  cout<<5;
+int main() {
+  srand(10);
+  list <int> listik(10,1);
+  list<int>::iterator it;
+  for (it=listik.begin();it!=listik.end();++it){
+      *it=rand()%10;}
+  for (it=listik.begin();it!=listik.end();++it){
+      cout<<*it<<' ';}
+  return 0;
 }
